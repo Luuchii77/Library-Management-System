97a6fcab8c213b66a378ac2823cca7d055dd5120
Lufuel Digal <lufueldigal77@gmail.com>
2025-05-25 21:29:33 +0800

    Delete Models directory

--- Models/Transaction.cs
+++
@@ -1,8 +0,0 @@
-﻿public class Transaction
-{
-    public int TransactionId { get; set; }
-    public int BookId { get; set; }
-    public int StudentId { get; set; }
-    public DateTime BorrowDate { get; set; }
-    public DateTime? ReturnDate { get; set; }
-}
